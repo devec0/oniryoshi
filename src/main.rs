c025061275428922dea023945641d5c09857804a
James Hebden <james@ec0.io>
2022-08-04 14:14:19 +1000

    working with reqwest

--- src/main.rs
+++ src/main.rs
@@ -17,31 +17,7 @@ use std::num::NonZeroU32;
 use nonzero_ext::*;
 use governor::{Quota, RateLimiter};
 
-use hyper::{Client, Uri};
-use hyper::body::to_bytes;
-use hyper::client::HttpConnector;
-use hyper_rustls::{HttpsConnectorBuilder, HttpsConnector};
-use hyper::client::connect::dns::GaiResolver;
-use rustls::{ClientConfig, Certificate, RootCertStore, OwnedTrustAnchor};
-use rustls::client::{ServerCertVerified, ServerCertVerifier, ServerName};
-
-struct SkipCertificationVerification;
-
-impl ServerCertVerifier for SkipCertificationVerification {
-    fn verify_server_cert(
-	&self,
-        _end_entity: &Certificate,
-	_intermediates: &[Certificate],
-	_server_name: &ServerName,
-	_scts: &mut dyn Iterator<Item = &[u8]>,
-	_ocsp_response: &[u8],
-	_now: SystemTime,
-    ) -> Result<ServerCertVerified, rustls::TLSError> {
-	Ok(ServerCertVerified::assertion())
-    }
-}
-
-async fn testurl(client: Client<HttpsConnector<HttpConnector<GaiResolver>>>, pb: ProgressBar, rx: spmc::Receiver<String>) -> Result<(), Box<dyn Error + Send + Sync + 'static>> {
+async fn testurl(client: reqwest::Client, pb: ProgressBar, rx: spmc::Receiver<String>) -> Result<(), Box<dyn Error + Send + Sync + 'static>> {
     //no certs
     loop {
 	tracing::trace!("waiting for work");
@@ -49,25 +25,16 @@ async fn testurl(client: Client<HttpsConnector<HttpConnector<GaiResolver>>>, pb:
 	if url == "close" { return Ok(()); }
 	tracing::trace!("sending request");
 	pb.inc(1);
-	let uri: Uri = match format!("{}/AirWatch", url).parse() {
-	    Ok(uri) => uri,
-	    Err(_) => continue
-	};
-	let resp = match client.get(uri).await {
-	    Ok(resp) => match to_bytes(resp).await {
-		Ok(bytes) => bytes,
+	let resp = match client.get(format!("{}/AirWatch", url)).send().await {
+	    Ok(resp) => match resp.text().await {
+		Ok(str) => str,
 		Err(_) => continue
 	    },
 	    Err(_) => continue
 	};
 
-	let body: String = match String::from_utf8(resp.to_vec()) {
-	    Ok(string) => string,
-	    Err(_) => continue
-	};
-
 	tracing::trace!("checking response");
-	if body.contains("/AirWatch/Login") {
+	if resp.contains("/AirWatch/Login") {
             pb.inc_length(1);
 	    println!("{}", url);
 	}
@@ -105,32 +72,11 @@ fn main() -> Result<(), Box<dyn Error + Send + Sync + 'static>> {
     let args: Vec<String> = env::args().collect();
     let filename: String = args[1].to_string();
 
-    let mut root_store = RootCertStore::empty();
-    root_store.add_server_trust_anchors(
-	webpki_roots::TLS_SERVER_ROOTS
-	    .0
-	    .iter()
-	    .map(|ta| {
-		OwnedTrustAnchor::from_subject_spki_name_constraints(
-		    ta.subject,
-		    ta.spki,
-		    ta.name_constraints,
-		)
-	    }),
-	    );
-    let mut tls: ClientConfig = ClientConfig::builder()
-	.with_safe_defaults()
-	.with_root_certificates(root_store)
-	.with_no_client_auth();
-    tls.dangerous().set_certificate_verifier(Arc::new(SkipCertificationVerification {}));
-
-    let https = HttpsConnectorBuilder::new()
-	.with_tls_config(tls)
-	.https_or_http()
-	.enable_http1()
-	.build();
-
-    let client = Client::builder().build::<_, hyper::Body>(https);
+    let client = reqwest::Client::builder()
+	.redirect(reqwest::redirect::Policy::none())
+	.danger_accept_invalid_certs(true)
+	.build()
+	.unwrap();
 
     let pb = ProgressBar::new(0);
     pb.set_draw_target(ProgressDrawTarget::stderr());
