b916216e824a5b95e880e3d416a8f3c99cfcba93
James Hebden <james@ec0.io>
2022-08-04 14:14:20 +1000

    rate and worker fixes

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ use std::env;
 use std::error::Error;
 use std::sync::Arc;
 use std::time::SystemTime;
+use std::time::Duration;
 
 use indicatif::{ProgressBar, ProgressDrawTarget, ProgressStyle};
 
@@ -17,34 +18,70 @@ use std::num::NonZeroU32;
 use nonzero_ext::*;
 use governor::{Quota, RateLimiter};
 
-async fn testurl(client: reqwest::Client, pb: ProgressBar, rx: spmc::Receiver<String>) -> Result<(), Box<dyn Error + Send + Sync + 'static>> {
+use clap::{Arg, App};
+
+struct Job {
+    host: String,
+    probe: String,
+    matchstr: String,
+}
+
+async fn testurl(pb: ProgressBar, rx: spmc::Receiver<Job>) -> Result<(), Box<dyn Error + Send + Sync + 'static>> {
+
+    let mut headers = reqwest::header::HeaderMap::new();
+    headers.insert(reqwest::header::USER_AGENT, reqwest::header::HeaderValue::from_static("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:95.0) Gecko/20100101 Firefox/95.0"));
+
     //no certs
+    let client = reqwest::Client::builder()
+	.default_headers(headers)
+	.timeout(Duration::from_secs(5))
+	.redirect(reqwest::redirect::Policy::none())
+	.danger_accept_invalid_hostnames(true)
+	.danger_accept_invalid_certs(true)
+	.build()
+	.unwrap();
+
     loop {
 	tracing::trace!("waiting for work");
-	let url = rx.recv().unwrap();
-	if url == "close" { return Ok(()); }
+	let job = rx.recv().unwrap();
+
 	tracing::trace!("sending request");
 	pb.inc(1);
-	let resp = match client.get(format!("{}/AirWatch", url)).send().await {
+
+	let get = client.get(format!("{}{}", job.host, job.probe));
+	let req = match get.build() {
+	    Ok(req) => req,
+	    Err(e) => {
+		//eprintln!("error building get request for {}: {:?}", job.host, e);
+		continue
+	    }
+	};
+	let resp = match client.execute(req).await {
 	    Ok(resp) => match resp.text().await {
 		Ok(str) => str,
-		Err(_) => continue
+		Err(e) => {
+		    //eprintln!("error matching on response: {:?}", e);
+		    continue
+		}
 	    },
-	    Err(_) => continue
+	    Err(e) => {
+		//eprintln!("error executing request for {}: {:?}", job.host, e);
+		continue
+	    }
 	};
 
 	tracing::trace!("checking response");
-	if resp.contains("/AirWatch/Login") {
+	if resp.contains(&job.matchstr) {
             pb.inc_length(1);
-	    println!("{}", url);
+	    println!("{}", job.host);
 	}
     }
 }
 
-async fn sendurl(filename: String, mut tx: spmc::Sender<String>) -> Result<(), Box<dyn Error + Send + Sync + 'static>> {
+async fn sendurl(filename: String, probe: String, matchstr: String, rate: u32,  mut tx: spmc::Sender<Job>) -> Result<(), Box<dyn Error + Send + Sync + 'static>> {
 
     //set rate limit
-    let mut lim = RateLimiter::direct(Quota::per_second(nonzero!(10000u32))); // Allow 50 units per second
+    let lim = RateLimiter::direct(Quota::per_second(std::num::NonZeroU32::new(rate).unwrap()));
 
     //open file
     let file = File::open(filename).await.unwrap();
@@ -54,31 +91,63 @@ async fn sendurl(filename: String, mut tx: spmc::Sender<String>) -> Result<(), B
     while let Some(line) = lines.next_line().await.unwrap() {
 	//request from url on line
 	tracing::trace!("sending url");
-	tx.send(line.to_string()).unwrap();
+	let msg = Job{
+	    host: line,
+	    probe: probe.clone(),
+	    matchstr: matchstr.clone(),
+	};
+	tx.send(msg).unwrap();
 	lim.until_ready().await;
-    }
-    for _n in 0..100000 {
-	tx.send("close".to_string());
     }
 
     Ok(())
 }
 
-//#[tokio::main(flavor = "current_thread")]
-fn main() -> Result<(), Box<dyn Error + Send + Sync + 'static>> {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error + Send + Sync + 'static>> {
 
     console_subscriber::init();
 
-    let args: Vec<String> = env::args().collect();
-    let filename: String = args[1].to_string();
-
-    let client = reqwest::Client::builder()
-	.redirect(reqwest::redirect::Policy::none())
-	.danger_accept_invalid_certs(true)
-	.build()
-	.unwrap();
+    let matches = App::new("oniryoshi")
+	.version("0.1.0")
+	.author("James Hebden <ec0@spooky.computer>")
+	.about("Scans endpoints for signatures")
+	.arg(Arg::with_name("in")
+	     .short("i")
+	     .long("in")
+	     .takes_value(true)
+	     .help("A file containing targets to scan in <scheme>://host:port format"))
+	.arg(Arg::with_name("rate")
+	     .short("r")
+	     .long("rate")
+	     .takes_value(true)
+	     .default_value("100")
+	     .help("Maximum in-flight requests per second"))
+	.arg(Arg::with_name("path")
+	     .short("p")
+	     .long("path")
+	     .takes_value(true)
+	     .help("HTTP path to request"))
+	.arg(Arg::with_name("match")
+	     .short("m")
+	     .long("match")
+	     .takes_value(true)
+	     .help("String to match in the response"))
+	.get_matches();
 
     let pb = ProgressBar::new(0);
+
+    let infile: String = matches.value_of("in").unwrap().to_string();
+    let probe: String = matches.value_of("path").unwrap().to_string();
+    let matchstr: String = matches.value_of("match").unwrap().to_string();
+    let rate = match matches.value_of("rate").unwrap().parse::<i32>() {
+	Ok(n) => n,
+	Err(_) => {
+	    pb.println("could not parse rate, using default of 100");
+	    100
+	}
+    };
+
     pb.set_draw_target(ProgressDrawTarget::stderr());
     pb.set_style(ProgressStyle::default_bar()
 		 .template("{spinner:.green} {elapsed} ({len}) {pos} {per_sec}")
@@ -94,21 +163,18 @@ fn main() -> Result<(), Box<dyn Error + Send + Sync + 'static>> {
 
     // start worker tasks
     rt.spawn(async move{
-	sendurl(filename, tx).await.unwrap();
+	sendurl(infile, probe, matchstr, rate.try_into().unwrap(), tx).await.unwrap();
     });
 
 
     let workers = FuturesUnordered::new();
-    let set = LocalSet::new();
-
-    for _n in 0..100000 {
+    for _n in 0..1000 {
 	let rx = rx.clone();
 	let pb = pb.clone();
-	let client = client.clone();
-	workers.push(set.spawn_local(async move { task::spawn_local( async move { testurl(client, pb, rx).await.unwrap()}).await.unwrap()}));
+	workers.push(task::spawn( async move { testurl(pb, rx).await.unwrap()}));
     }
 
-    rt.block_on(set.run_until(workers.for_each(|_| async { () })));
+    let _results: Vec<_> = workers.collect().await;
 
     Ok(())
 }
